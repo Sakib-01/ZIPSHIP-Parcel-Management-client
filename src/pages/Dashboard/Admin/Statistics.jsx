3cf13e475237733200702a1b89821d8bdb081edc
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-18 19:41:49 +0600

    add state card

--- src/pages/Dashboard/Admin/Statistics.jsx
+++ src/pages/Dashboard/Admin/Statistics.jsx
@@ -2,9 +2,23 @@ import React, { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { Calendar } from "react-date-range";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "../../../components/LoadingSpinner";
 
 const Statistics = () => {
   const axiosSecure = useAxiosSecure();
+
+  const {
+    data: cards = {},
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["cards"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-card");
+      return res.data;
+    },
+  });
 
   const [chartData, setChartData] = useState({
     categories: [],
@@ -44,11 +58,47 @@ const Statistics = () => {
       align: "center",
     },
   };
+  if (isLoading) return <LoadingSpinner />;
+  console.log(cards);
   return (
     <div>
       <h2 className="text-3xl flex mx-auto justify-center items-center text-primary underline mb-10 pt-2">
         Admin statistic
       </h2>
+      <div className="stats shadow grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="stat place-items-center">
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${cards?.revinue}</div>
+        </div>
+
+        <div className="stat place-items-center">
+          <div className="stat-title"> Total Users</div>
+          <div className="stat-value ">{cards?.users}</div>
+        </div>
+
+        <div className="stat place-items-center">
+          <div className="stat-title">Total Delivery Man</div>
+          <div className="stat-value">{cards?.deliveryMan}</div>
+        </div>
+      </div>
+
+      <div className="stats shadow grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-5">
+        <div className="stat place-items-center">
+          <div className="stat-title">Total Parcel Booked</div>
+          <div className="stat-value">{cards?.booking}</div>
+        </div>
+
+        <div className="stat place-items-center">
+          <div className="stat-title">Parcel Delivered</div>
+          <div className="stat-value">{cards?.parcelDelivered}</div>
+        </div>
+
+        <div className="stat place-items-center">
+          <div className="stat-title">Parcel On Way</div>
+          <div className="stat-value">{cards?.parcelOnWay}</div>
+        </div>
+      </div>
+
       <div className="flex flex-col md:flex-row justify-center items-center gap-10">
         <div className="w-8/12 text-text bg-slate-100 p-10">
           <Chart
