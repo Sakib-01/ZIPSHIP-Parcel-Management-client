f7f8052d4d1d8d186faf123b922585e5209d30c4
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-18 19:07:06 +0600

    add aos

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Hero from "./Hero";
 import TopDeliveryman from "./TopDeliveryMan/TopDeliveryman";
 import UsersCount from "./UsersCount/UsersCount";
@@ -7,22 +9,25 @@ import About from "./About/About";
 import Review from "./Review/Review";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({ duration: 500, once: true });
+  }, []);
   return (
     <div className="space-y-20">
-      <div>
+      <div data-aos="fade-up">
         <Hero />
         <UsersCount />
       </div>
-      <div>
+      <div data-aos="fade-right">
         <Frature />
       </div>
-      <div>
+      <div data-aos="fade-left">
         <About />
       </div>
-      <div>
+      <div data-aos="zoom-in">
         <TopDeliveryman />
       </div>
-      <div>
+      <div data-aos="zoom-in">
         <Review />
       </div>
     </div>
